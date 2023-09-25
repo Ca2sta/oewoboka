ba62ce36cc05faa69f1a2dd1f3db33993d5ba2f2
= <=>
2023-09-25 19:32:10 +0900

    [FEAT] Carousel collectionview 구현

---
+++ oewoboka/oewoboka/Scenes/FeaturePage/FeatureViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FeatureViewModel.swift
+//  oewoboka
+//
+//  Created by SeoJunYoung on 2023/09/25.
+//
+
+import Foundation
+import UIKit
+
+final class FeatureViewModel {
+    
+    let minItemSpacing: CGFloat = 40
+    let cellSize = CGSize(width: UIScreen.main.bounds.width - 100, height: UIScreen.main.bounds.height - 400)
+    var previousIndex = 0
+}
