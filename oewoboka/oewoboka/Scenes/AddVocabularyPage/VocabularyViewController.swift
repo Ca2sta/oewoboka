447a02a57779dc919c5a825ebb4cd24cd389bc9f
gunnie <gunnieee91@gmail.com>
2023-10-04 14:58:46 +0900

    [feat]추가하기UI구현

--- oewoboka/oewoboka/Scenes/AddVocabularyPage/VocabularyViewController.swift
+++ oewoboka/oewoboka/Scenes/AddVocabularyPage/VocabularyViewController.swift
@@ -6,15 +6,78 @@
 //
 
 import UIKit
+import SnapKit
 
-class VocabularyViewController: UIView {
+final class VocabularyViewController: UIViewController {
+    
+    
+    private let divider: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray
+        return view
+    }()
+    
+    // TextField 란
+    private let wordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.placeholder = "단어장 이름을 입력해 주세요!"
+        return textField
+    }()
+    
+    private let descriptionTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .white
+        textField.placeholder = "단어장 설명을 입력해 주세요!(선택)"
+        return textField
+    }()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
-}
+    
+    // '생성' 버튼
+    private let addVocablaryButton: UIButton = {
+            let button = UIButton(type: .system)
+            button.setTitle("생성", for: .normal)
+            button.addTarget(self, action: #selector(didTapaddVocablaryButton), for: .touchUpInside)
+            return button
+        }()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            view.addSubview(wordTextField)
+            view.addSubview(descriptionTextField)
+            view.addSubview(addVocablaryButton)
+            
+            setupConstraints()
+        }
+        
+     // TextField 란 설정
+        private func setupConstraints() {
+            wordTextField.snp.makeConstraints { (make) in
+                make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+                make.left.equalTo(view).offset(20)
+                make.right.equalTo(view).offset(-20)
+                make.height.equalTo(40)
+            }
+            
+            descriptionTextField.snp.makeConstraints { (make) in
+                make.top.equalTo(wordTextField.snp.bottom).offset(20)
+                make.left.equalTo(view).offset(20)
+                make.right.equalTo(view).offset(-20)
+                make.height.equalTo(40)
+            }
+       // '생성' 버튼 설정
+            addVocablaryButton.snp.makeConstraints { (make) in
+                make.top.equalTo(descriptionTextField.snp.bottom).offset(20)
+                make.centerX.equalTo(view)
+            }
+        }
+        
+        @objc private func didTapaddVocablaryButton() {
+            let vocabularyVC = AddVocabularyViewController()
+                    self.present(vocabularyVC, animated: true, completion: nil)
+                }
+       
+        }
