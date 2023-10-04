e2e12d8bffef3a681a347f3b685a7b56301f9daa
gunnie <gunnieee91@gmail.com>
2023-10-04 14:53:03 +0900

    [feat]추가하기UI구현

---
+++ oewoboka/oewoboka/Scenes/AddVocabularyPage/VocabularyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VocabularyViewController.swift
+//  oewoboka
+//
+//  Created by APPLE M1 Max on 2023/10/04.
+//
+
+import UIKit
+
+class VocabularyViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
